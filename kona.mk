d0e2ada99bf5e8fbfcff5314e8a6bb1bb3fd5340
Ramii Ahmed <ramy@ahmedramy.com>
2022-04-01 22:32:23 +0000

    sm8250-common: Build vendor.qti.hardware.servicetracker@1.2 HIDL
    Signed-off-by: drkphnx <dark.phnx12@gmail.com>

--- kona.mk
+++ kona.mk
@@ -502,6 +502,11 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2-service \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
